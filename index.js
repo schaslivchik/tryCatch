d66f1b96bf9e1384b74b57d650e6ed35347d0979
Mikhail <schaslivchikm@gmail.com>
2022-02-03 20:57:55 +0200

    homework

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 'use strict'
 
 function pow(base, exponent) {
+  
+  if(typeof base !== 'number'){
+    throw new TypeError('base must be number')
+  }
+ 
+  if(exponent <= 0 || base <= 0){
+    throw new Error('numbers must be >= 0')
+  }
+
+  if(!Number.isInteger(exponent) || typeof exponent !== 'number'){
+    throw new TypeError('exponent must be integer number')
+  }
+  
   if (exponent === 0) {
     return 1;
   }
   return base * pow(base, exponent - 1);
 }
-console.log("pow(2,3) :>> ", pow(2, 3));
+
+
+try {
+  console.log('object :>> ', pow(-2, 2));
+} catch(error){
+  console.log('e :>> ', error);
+}
